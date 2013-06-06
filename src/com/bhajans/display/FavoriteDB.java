0c1284b56de3a4eb2f83b2e46897fc06f31ffccd
Balaji Subramanian <balaji_mani87@yahoo.co.uk>
2013-06-05 19:29:37 -0500

    Update FavoriteDB.java

--- src/com/bhajans/display/FavoriteDB.java
+++ src/com/bhajans/display/FavoriteDB.java
@@ -10,44 +10,44 @@ import com.bhajans.lookup.LookUpData;
 public class FavoriteDB {
 	public static CacheDB cacheDB = null;
 	public static CacheDB getCacheDB() {
-		if(cacheDB == null)
-		   cacheDB = new CacheDB(getContext());
+	    if(cacheDB == null)
+	        cacheDB = new CacheDB(getContext());
 		return cacheDB;
 	}
 	
 	public static void setContext(Context context) {
-		LookUpData.context = context;
+	    LookUpData.context = context;
 	}
 	  
 	public static Context getContext() {
-		return LookUpData.context;
+	    return LookUpData.context;
 	}
 	  
 	public static void addBhajan(String bhajan) {
-		ArrayList<String> bhajanToAdd = new ArrayList<String>();
-		bhajanToAdd.add(bhajan);
-		FavoriteDB.getCacheDB().performOperation("INSERT", "favorites", bhajanToAdd);    
+	    ArrayList<String> bhajanToAdd = new ArrayList<String>();
+	    bhajanToAdd.add(bhajan);
+	    FavoriteDB.getCacheDB().performOperation("INSERT", "favorites", bhajanToAdd);    
 	}
 	  
 	public static void removeBhajan(String bhajan) {
-		ArrayList<String> bhajanToAdd = new ArrayList<String>();
-		bhajanToAdd.add(bhajan);
-		FavoriteDB.getCacheDB().performOperation("DELETE", "favorites", bhajanToAdd);    
+	    ArrayList<String> bhajanToAdd = new ArrayList<String>();
+	    bhajanToAdd.add(bhajan);
+	    FavoriteDB.getCacheDB().performOperation("DELETE", "favorites", bhajanToAdd);    
 	}
 	  
 	public static int selectBhajan(String bhajan) {
-		ArrayList<String> bhajanToAdd = new ArrayList<String>();
-		bhajanToAdd.add(bhajan);
-		int count = FavoriteDB.getCacheDB().fetchCount("favorites", bhajan);
-		return count;
+	    ArrayList<String> bhajanToAdd = new ArrayList<String>();
+	    bhajanToAdd.add(bhajan);
+	    int count = FavoriteDB.getCacheDB().fetchCount("favorites", bhajan);
+	    return count;
 	}
 	  
 	public static ArrayList<String> fetchBhajans(){
-		ArrayList<String> favBhajans = (ArrayList<String>) FavoriteDB.getCacheDB().fetchData("favorites");
-		if(favBhajans.size()==0) { 
-			favBhajans.add("No Favorites");	
-		}
-		return favBhajans; 
+	    ArrayList<String> favBhajans = (ArrayList<String>) FavoriteDB.getCacheDB().fetchData("favorites");
+	    if(favBhajans.size()==0) { 
+		favBhajans.add("No Favorites");	
+	    }
+	    return favBhajans; 
 	}
 
 
